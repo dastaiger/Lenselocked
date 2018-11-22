43369befc5fbcfa01575ff7da9b868963c8a3656
Daniel <danielstaiger@>
2018-11-22 15:00:35 +0100

    first web app

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hi Test")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil) //nil = use buildin MUX
+
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Welcome to my Site! </h1>")
 }
