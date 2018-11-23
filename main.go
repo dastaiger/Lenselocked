c6d8abafcab1267a24feba7f02cad9ff80938248
Daniel <danielstaiger@>
2018-11-23 09:49:58 +0100

    added Gorilla Mux

--- main.go
+++ main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil) //nil = use buildin MUX
+
+	r := mux.NewRouter()
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/", home)
+	http.ListenAndServe(":3000", r) //nil = use buildin MUX
 
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	w.Header().Set("Host", "Wurst")
+	fmt.Fprint(w, "contact me @ <a href=\"mailto:bla@spam.de\"> mailaddr </a>.")
 
-	if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "contact me @ <a href=\"mailto:bla@spam.de\"> mailaddr </a>.")
-	} else if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1> hiho </h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "404 - not there..")
-	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> hiho, this is home :)</h1>")
 }
