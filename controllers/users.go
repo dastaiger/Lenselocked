721eabcd72598633a524003bb298147c1d8d1b34
Daniel <danielstaiger@>
2018-12-03 09:38:02 +0100

    added ParseForm from the Signup Page

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"lenslocked.com/views"
@@ -17,7 +18,19 @@ func NewUsers() *Users {
 	}
 }
 
+// New shows the User Signup Form
+//
+// /Signup GET
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
 	u.NewView.Render(w, nil)
+}
+
+// Create handles the creation of a new User
+//
+// /Signup POST
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Fprintf(w, "this is a Temp Mesage: %v, %v", r.PostFormValue("email"), r.PostFormValue("Password"))
 }
--- main.go
+++ main.go
@@ -22,10 +22,11 @@ func main() {
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/", home)
-	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", usersC.New)
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/faq", faq).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r) //nil = use buildin MUX
 
 }
