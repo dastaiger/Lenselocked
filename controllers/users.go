16cda979d1b36afe4987d8ac3d1d1e64c7ece67f
Daniel <danielstaiger@>
2018-12-03 10:30:09 +0100

    7.7-Added-Gorilla-SCHEMA

--- controllers/users.go
+++ controllers/users.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"lenslocked.com/views"
 )
 
+// Set a Decoder instance as a package global, because it caches
+// meta-data about structs, and an instance can be shared safely.
+var decoder = schema.NewDecoder()
+
 type Users struct {
 	NewView *view.View
+}
+
+type SignUp struct {
+	Email    string `schema:"email,required"`
+	Password string `schema:"password,required"`
 }
 
 //NewUsers can panic -> only use this function during setup
@@ -30,7 +40,15 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // /Signup POST
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
 
-	fmt.Fprintf(w, "this is a Temp Mesage: %v, %v", r.PostFormValue("email"), r.PostFormValue("Password"))
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	var user SignUp
+	if err := decoder.Decode(&user, r.PostForm); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "this is a Temp Mesage: %v, %v \n %v", r.PostFormValue("email"), r.PostFormValue("Password"), user)
 }
